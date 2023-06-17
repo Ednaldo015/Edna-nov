22a72cfd3e6a5db9a0660a76217ac070f5d50bef
Ednaldo015 <132470028+Ednaldo015@users.noreply.github.com>
2023-06-17 03:42:34 -0300

    1 file modified, 5 added

---
+++ tests/saveCartItem.test.js
@@ -0,0 +1,23 @@
+const saveCartItems = require('../helpers/saveCartItems');
+
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    setItem: jest.fn(),
+  },
+});
+
+describe('3 - Teste a função saveCartItems', () => {
+  it('Deve chamar o método localStorage.setItem', () => {
+    const cartItems = '<ol><li>Item</li></ol>';
+
+    saveCartItems(cartItems);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+
+  it('Deve chamar o método localStorage.setItem com o argumento correto', () => {
+    const cartItems = '<ol><li>Item</li></ol>';
+
+    saveCartItems(cartItems);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', cartItems);
+  });
+});
